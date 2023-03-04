07499c40de0dcc289ee930f11ad93ac5a7e83cdd
Swayam Zurmure <119281810+swayam2402@users.noreply.github.com>
2023-03-04 22:48:41 +0530

    Add files via upload

---
+++ temperature convertor/temperature_convertor3.js
@@ -0,0 +1,17 @@
+var par = document.querySelector("#par");
+    let temprature = () => {
+        let opt = document.querySelector("#unit");
+        let display = document.querySelector(".value");
+        var a = document.querySelector(".enter").value;
+        if (opt.value == "Celsius") {
+            console.log(a, "C to F", opt.value)
+            let F = a * (9 / 5) + 32
+            display.innerHTML = F.toFixed(3) + " F"
+            
+        } else {
+            console.log(a, "F to C", opt.value)
+            let C = (a - 32) * (5 / 9)
+            display.innerHTML = C.toFixed(3) + " ºC"
+           
+        }
+    }
